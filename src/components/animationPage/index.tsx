096177778ff34b18aca527febd9a3bd74564fb7e
sm-karthi <karthikeyan24022007@gmail.com>
2025-09-08 11:42:17 +0530

    Change the correct alignment

--- src/components/animationPage/index.tsx
+++ src/components/animationPage/index.tsx
@@ -67,99 +67,112 @@ export function AnimationPage() {
 
 
   return (
-    <section className="bg-black px-6 md:px-18 py-40 relative overflow-x-hidden">
+    <section className="bg-black px-6 md:px-18 py-40 overflow-x-hidden">
 
-      <div className="bg-[#ede7dd] rounded-2xl px-6 md:px-74 text-center py-18 md:py-26 flex flex-col justify-center items-center">
+      <div className="flex justify-center">
 
-        <h3 className="text-3xl">A network of like-minded specialists</h3>
+        <div className="xl:w-[1405px] relative">
 
-        <p className="text-xl mt-10 leading-relaxed">
-          At Bluetech, you don&apos;t work alone. You&apos;re part of a close-knit
-          community where knowledge sharing, growth, and fun are a given.
-        </p>
+          <div>
 
-        <p className="font-light mt-10">
-          Workshops, meet-ups and knowledge sessions
-        </p>
+            <div className="bg-[#ede7dd] rounded-2xl px-6 md:px-74 text-center py-18 md:py-26 flex flex-col justify-center items-center">
 
-        <p className="font-light mt-3">
-          Team outings, celebrating successes and connecting
-        </p>
+              <h3 className="text-3xl">A network of like-minded specialists</h3>
 
-        <p className="font-light mt-3">
-          Personal contact, also outside of work
-        </p>
+              <p className="text-xl mt-10 leading-relaxed">
+                At Bluetech, you don&apos;t work alone. You&apos;re part of a close-knit
+                community where knowledge sharing, growth, and fun are a given.
+              </p>
 
-        <div className="mt-5 md:mt-10">
-          <Btn
-            text="More about our community"
-            bgColor="bg-[#ede7dd]"
-            borderColor="border border-black"
-            textColor="text-black"
-            hoverBgColor="hover:bg-black"
-            hoverTextColor="hover:text-white"
-          />
+              <p className="font-light mt-10">
+                Workshops, meet-ups and knowledge sessions
+              </p>
+
+              <p className="font-light mt-3">
+                Team outings, celebrating successes and connecting
+              </p>
+
+              <p className="font-light mt-3">
+                Personal contact, also outside of work
+              </p>
+
+              <div className="mt-5 md:mt-10">
+                <Btn
+                  text="More about our community"
+                  bgColor="bg-[#ede7dd]"
+                  borderColor="border border-black"
+                  textColor="text-black"
+                  hoverBgColor="hover:bg-black"
+                  hoverTextColor="hover:text-white"
+                />
+
+              </div>
+
+            </div>
+
+            <Image
+              src={img_1}
+              alt="Image 1"
+              className="md:w-44 w-24 top-19 absolute md:top-4 2xl:top-10 left-0 scroll-image1"
+            />
+
+            <Image
+              src={img_2}
+              alt="Image 1"
+              className="w-20 md:w-32 absolute top-110 md:top-78 2xl:top-80 left-0 md:left-16 scroll-image1"
+            />
+
+            <Image
+              src={img_3}
+              alt="Image 1"
+              className="md:w-38 w-20 absolute bottom-16 md:bottom-2 2xl:bottom-1 left-8 scroll-image1"
+            />
+
+            <Image
+              src={img_4}
+              alt="Image 1"
+              className="md:w-44 w-30 absolute bottom-8 2xl:bottom-1 2xl:right-10 right-20 md:right-16"
+            />
+
+            <Image
+              src={img_5}
+              alt="Image 1"
+              className="md:w-32 w-26 absolute bottom-4 2xl:bottom-1 left-30 md:left-66 2xl:left-70 scroll-image2"
+            />
+
+            <Image
+              src={img_6}
+              alt="Image 1"
+              className="md:w-52 w-48 absolute top-22 md:top-0 2xl:-top-20 right-30 md:right-120 scroll-image2"
+            />
+
+            <Image
+              src={img_7}
+              alt="Image 1"
+              className="md:w-58 w-59 absolute bottom-10 2xl:right-50 right-10 md:right-50 scroll-image3"
+            />
+
+            <Image
+              src={img_8}
+              alt="Image 1"
+              className="md:w-60 w-64 absolute md:top-46 top-70 2xl:top-50 -right-30 md:-right-40 scroll-image3"
+            />
+
+            <Image
+              src={img_9}
+              alt="Image 1"
+              className="md:w-34 w-30 absolute top-24 md:top-10 2xl:top-2 right-6 scroll-image3"
+            />
+
+
+          </div>
 
         </div>
 
       </div>
 
-      <Image
-        src={img_1}
-        alt="Image 1"
-        className="md:w-44 w-24 top-19 absolute md:top-28 left-0 scroll-image1"
-      />
-
-      <Image
-        src={img_2}
-        alt="Image 1"
-        className="w-20 md:w-32 absolute top-110 md:top-98 left-0 md:left-16 scroll-image1"
-      />
-
-      <Image
-        src={img_3}
-        alt="Image 1"
-        className="md:w-38 w-20 absolute bottom-16 left-8 scroll-image1"
-      />
-
-      <Image
-        src={img_4}
-        alt="Image 1"
-        className="md:w-60 w-30 absolute bottom-8 right-20 md:right-26"
-      />
-
-      <Image
-        src={img_5}
-        alt="Image 1"
-        className="md:w-38 w-26 absolute bottom-4 left-30 md:left-86 scroll-image2"
-      />
-
-      <Image
-        src={img_6}
-        alt="Image 1"
-        className="md:w-68 w-48 absolute top-22 right-30 md:right-120 scroll-image2"
-      />
-
-      <Image
-        src={img_7}
-        alt="Image 1"
-        className="md:w-78 w-59 absolute bottom-10 right-10 md:right-74 scroll-image3"
-      />
-
-      <Image
-        src={img_8}
-        alt="Image 1"
-        className="md:w-78 w-64 absolute top-70 -right-30 md:-right-50 scroll-image3"
-      />
-
-      <Image
-        src={img_9}
-        alt="Image 1"
-        className="md:w-40 w-30 absolute top-24 right-6 scroll-image3"
-      />
-
     </section>
-    
+
   );
 }
 
